074ce8b5a43f7570e6af7c44c92dc733542fb576
wenewzhang <wenewboy@gmail.com>
2020-02-03 23:39:20 +0800

    add vuetify

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,15 @@
 module.exports = {
-  publicPath: '',
-  outputDir: 'cordova_app/www',
-  assetsDir: undefined,
-  runtimeCompiler: undefined,
-  productionSourceMap: undefined,
-  parallel: undefined,
-  css: undefined,
-  pluginOptions: {
-    i18n: {
-      locale: 'en',
-      fallbackLocale: 'en',
-      localeDir: 'locales',
-      enableInSFC: false
+  "publicPath": "",
+  "outputDir": "cordova_app/www",
+  "pluginOptions": {
+    "i18n": {
+      "locale": "en",
+      "fallbackLocale": "en",
+      "localeDir": "locales",
+      "enableInSFC": false
     }
-  }
+  },
+  "transpileDependencies": [
+    "vuetify"
+  ]
 }
