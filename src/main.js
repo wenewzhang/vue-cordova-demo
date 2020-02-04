a580ff69a6c499a2d70f508ef3a2d8620ee0286f
wenewzhang <wenewboy@gmail.com>
2020-02-04 14:42:43 +0800

    add vuetify

--- src/main.js
+++ src/main.js
@@ -5,8 +5,23 @@ import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
-new Vue({
-  i18n,
-  vuetify,
-  render: h => h(App)
-}).$mount('#app')
+
+const init = () => {
+  new Vue({
+    vuetify,
+    i18n,
+    render: h => h(App)
+  }).$mount('#app')
+};
+// Wait for the deviceready event to start the render
+document.addEventListener("deviceready", () => {
+  // eslint-disable-next-line
+  console.log("Ready, Render the App");
+  init();
+});
+
+// If we are not in Cordova, manually trigger the deviceready event
+const isCordovaApp = (typeof window.cordova !== "undefined");
+if (!isCordovaApp){
+  document.dispatchEvent(new CustomEvent("deviceready", {}));
+}
